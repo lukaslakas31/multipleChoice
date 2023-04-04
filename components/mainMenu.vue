00ccb0f8bb48de2fb65a077d89ee5020122d7e20
Lukaslakas31 <lukejuniel@gmail.com>
2023-04-04 16:20:48 +0800

    r

--- components/mainMenu.vue
+++ components/mainMenu.vue
@@ -1,8 +1,36 @@
 <template>
-
+<div> 
+    <h1> Quiz<span>dom</span></h1>
+    <button @click="startQuiz">Start</button>
+    <button @click="showHelp">Help</button>
+    <div v-if="helpVisible" class="help-box">
+        <p> How to Play: </p>
+        <ul>
+        <li>Select an answer from the options provided</li>
+        <li>Click the "Next" button to move to the next question</li>
+        <li>Repeat until you've answered all the questions</li>
+        <li>Click the "Submit" button to see your score</li>
+      </ul>
+    </div>
+</div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      helpVisible: false
+    }
+  },
+  methods: {
+    startQuiz() {
+      this.$router.push('/questionPage')
+    },
+    showHelp() {
+      this.helpVisible = true
+    }
+  }
+}
 </script>
 
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-  
+  <MainMenu/>
 </template>
 
 <script>
+import MainMenu from '~/components/mainMenu.vue';
+
 export default {
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { MainMenu }
 }
 </script>
