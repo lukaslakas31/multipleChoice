555f4a7860ee6d4a1b976a28e53d912426c7cd58
Lukaslakas31 <lukejuniel@gmail.com>
2023-04-11 08:44:09 +0800

    added icons, props

--- components/multipleChoice.vue
+++ components/multipleChoice.vue
@@ -44,31 +44,13 @@
 
 <script>
 export default {
+  props:['questions'],
   data() {
     return {
       mainMenu: false,
       quizGame: false,
       showHow: false,
       showAnswer: false,
-      questions: [
-        {
-          question: "'OS' Computer abbreviation usually means?",
-          answers: [
-            { text: "Open Software", correct: false },
-            { text: "Operating System", correct: true },
-            { text: "Optical Sensor", correct: false },
-          ]
-        },
-        {
-          question: "How many bits is a byte?",
-          answers: [
-            { text: "4", correct: false },
-            { text: "8", correct: true },
-            { text: "16", correct: false },
-          ]
-        },
-        // add questions
-      ],
       currentQuestionIndex: 0,
       score: 0,
       selectedAnswer: null,
