811ff6e7e233fd0aaa4518983a12fc6a6eaef26e
AngeloEdrosa <angelo21edrosa8@gmail.com>
2023-04-11 08:32:08 +0800

    done

--- components/multipleChoice.vue
+++ components/multipleChoice.vue
@@ -1,44 +1,44 @@
 <template>
-  <div>
+  <div class="content">
     <!-- main menu area  -->
-    <b-container v-if="!mainMenu">
-      <b-row>
-        <b-col>
-          <h1> Multiple Choice Quiz </h1>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
+    <div class="main" v-if="!mainMenu">
+          <h1 id="title">
+            <span class="quiz-text">Quiz</span>Dom
+          </h1>
           <b-button class="playButton" @click="mainMenu = true">START</b-button>
-        </b-col>
-        <b-col>
           <b-button class="howButton" @click="showHow = true">HELP</b-button>
-        </b-col>
-      </b-row>
-    </b-container>
-    <b-modal v-model="showHow" title="How to Play" hide-footer no-close-on-esc no-close-on-backdrop>
+    </div>
+    <b-modal v-model="showHow" title="How to Play" hide-footer>
       <p> Choose the best answer in every question! </p>
     </b-modal>
     <!-- question area  -->
-    <b-container v-if="mainMenu">
-      <div v-if="!quizGame">
-        <h3>{{currentQuestionNumber}} / {{questions.length}} {{ currentQuestion.question }}</h3>
-        <div v-for="(answer, answerIndex) in currentQuestion.answers" :key="answerIndex">
-          <b-button class="choiceButton" @click="checkAnswer(answer)">{{ answer.text }}</b-button>
+    <div class="question" v-if="mainMenu">
+      <div class="questionpart" v-if="!quizGame">
+        <div class="tops">
+          <h3 class="qnum">{{currentQuestionNumber}} / {{questions.length}} </h3>
+          <i class="fa-solid fa-clock"></i>
         </div>
-        <!-- modal correct/wrong area  -->
-        <b-modal v-model="showAnswer" title="Answer" hide-footer hide-header-close  no-close-on-esc no-close-on-backdrop>
-          <p v-if="isCorrectAnswer">Correct!</p>
-          <p v-else>Incorrect!</p>
-          <b-button @click="nextQuestion">Next</b-button>
-        </b-modal>
+          <div class="qanda">
+              <h3 class="quest">{{ currentQuestion.question }}</h3>
+              <div class="choices">
+              <div v-for="(answer, answerIndex) in currentQuestion.answers" :key="answerIndex">
+                <b-button class="choiceButton" :id="answerIndex" @click="checkAnswer(answer)">{{ answer.text }}</b-button>
+              </div>
+              <!-- modal correct/wrong area  -->
+                <b-modal class="result" v-model="showAnswer" title="Answer" hide-footer hide-header-close  no-close-on-esc no-close-on-backdrop>
+                  <p v-if="isCorrectAnswer">Correct!</p>
+                  <p v-else>Incorrect!</p>
+                  <b-button @click="nextQuestion">Next</b-button>
+                </b-modal>
+          </div>
+        </div>
       </div>
       <!-- modal score area -->
       <b-modal v-model="quizGame" title="Done!" hide-footer hide-header-close  no-close-on-esc no-close-on-backdrop >
         <p>Score: {{ score }} out of {{ questions.length }}</p>
         <b-button class="menuButton" @click="backMenu">Main Menu</b-button>
       </b-modal>
-    </b-container>
+    </div>
   </div>
 </template>
 
@@ -120,5 +120,137 @@ export default {
 </script>
 
 <style>
+  html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #461a42;
+}
+
+.main{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  display: flex;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  gap: 80px;
+}
+
+#title {
+  font-family: cursive;
+  font-weight: 400;
+  font-size: 150px;
+  color: #eca82c;
+  text-align: center;
+  line-height: 0.8;
+}
+
+  .quiz-text {
+    color: #FFFFFF;
+  }
+
+  .playButton{
+    color: white;
+    background-color: rgb(21, 199, 113);
+    font-size: 30px;
+    border-radius: 15px;
+    box-shadow: 0px 17px 0px 0px rgb(77, 115, 138);
+    border: none;
+    padding: 5px 25px;
+    cursor: pointer;
+    letter-spacing: 3px;
+  }
+
+  .playButton:hover{
+    background-color: rgb(14, 169, 94);
+  }
+
+  .playButton:active{
+    transform: translateY(7px);
+    box-shadow: 0px 10px 0px 0px rgb(67, 88, 93);
+  }
+  .howButton{
+    color: white;
+    background-color: rgb(21, 199, 113);
+    font-size: 30px;
+    border-radius: 15px;
+    box-shadow: 0px 17px 0px 0px rgb(77, 115, 138);
+    border: none;
+    padding: 5px 25px;
+    cursor: pointer;
+    letter-spacing: 3px;
+  }
+
+  .howButton:hover{
+    background-color: rgb(14, 169, 94);
+  }
+
+  .howButton:active{
+    transform: translateY(7px);
+    box-shadow: 0px 10px 0px 0px rgb(67, 88, 93);
+  }
+
+  .question{
+    min-height: 100vh;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-content: space-between;
+  }
+
+  .tops{
+    display: flex;
+    justify-content: space-between;
+    margin: 15px;
+  }
+
+  .qnum{
+    color:#FFFFFF;
+    font-size: 40px;
+  }
+
+  i{
+    font-size: 80px;
+    border: solid #FFFFFF;
+    border-radius: 50%;
+  }
+
+  .quest{
+    display: flex;
+    justify-content: center;
+    color: #FFFFFF;
+    font-size: 50px;
+  }
+
+  .qanda{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 85vh;
+    gap: 100px;
+  }
+
+  .qanda .choices{
+    border: solid rgb(5, 207, 73);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    width: 100%;
+    margin-top: 100px;
+  }
+
+  #answerIndex{
+    font-size: 50px;
+  }
+
+  .modal-dialog {
+    background-color: #393937;
+  }
 
 </style>
